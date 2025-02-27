bfa840d238a5a88bfa0fe5d9394bff64fc458611
Juan Zago Rodríguez <36072973+JuanZagoR@users.noreply.github.com>
2025-02-27 16:22:27 -0600

    Primera versión de script de instalación de Docker

---
+++ instalador-docker.sh
@@ -0,0 +1,27 @@
+# Desinstalar paquetes conflictivos
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Cargar clave GPG de Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Añadir el repositorio de Docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Instalar Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo "Instalación de Docker completada."
+sleep 1
+
+# Comprobar la instalación de Docker
+echo "Probando Docker..."
+sleep 1
+sudo docker --version
+sleep 1
+sudo docker run hello-world
+echo "Docker está instalado y funcionando." 
